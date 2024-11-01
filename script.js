9e29bd20956a4f5cc2ddf1ea4fb541f3028bdb1f
dune123 <96974209+dune123@users.noreply.github.com>
2024-11-01 10:30:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 function validEmail(str) {
   //your JS code here.
+	const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+	return emailPattern.test(str);
 }
 
 // Do not change the code below.
-const str = prompt("Enter an email address.");
+const str = prompt("Enter an email address."); 
 alert(validEmail(str));
